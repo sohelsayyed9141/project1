c0e2c4c295f8658fcceeeae3deda08169b7727a6
sohelsayyed9141 <sayyed.sohel9141@gmail.com>
2023-12-21 12:05:06 +0530

    sohel syd

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var app = require('../index.js');describe('GET /', function() {
- it('respond with hello im sohel sayyed', function(done) { //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello im sohel sayyed', done);
+ it('respond with hello im sohel syd', function(done) { //navigate to root and check the the response is "hello world"
+ request(app).get('/').expect('hello im sohel syd', done);
  });
 });
